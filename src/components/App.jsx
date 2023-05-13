2bcc47d9e2bf40df7062f7b79c994947886d12d5
M-Farmaha <115003821+M-Farmaha@users.noreply.github.com>
2023-05-13 03:33:33 +0300

    task 1.1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,15 +6,7 @@ import * as Styled from './styled';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      { id: 'id-5', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-6', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-7', name: 'Rosie Simpson', number: '459-12-56' },
-    ],
+    contacts: [],
     filter: '',
   };
 
